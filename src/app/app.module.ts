b6322e7b059e4b870322e2bf88118de7e0696fc7
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-09-07 11:33:14 -0300

    se creo componente de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RedSocialLoginComponent } from './red-social-login/red-social-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedSocialLoginComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
