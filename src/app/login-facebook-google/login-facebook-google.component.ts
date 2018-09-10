613fd408489454c7ef2fe6efd7d9a7ecec736b97
LeslieInostroza <leslie.ino@gmail.com>
2018-09-10 13:32:54 -0300

    se crea login de face

---
+++ src/app/login-facebook-google/login-facebook-google.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-facebook-google',
+  templateUrl: './login-facebook-google.component.html',
+  styleUrls: ['./login-facebook-google.component.css']
+})
+export class LoginFacebookGoogleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
