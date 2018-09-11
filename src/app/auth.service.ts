00495b3394bcc9746b83dd4a4cf624203ed399f9
LeslieInostroza <leslie.ino@gmail.com>
2018-09-11 11:29:58 -0300

    firebase instaldo

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user: Observable<firebase.User>;
 
-  constructor(private firebaseAuth: AngularFireAuth) { }
+  constructor(private firebaseAuth: AngularFireAuth) { 
+    this.user = firebaseAuth.authState;
+  }
+  signup(email: string, password: string) {
+    return this.firebaseAuth.auth
+      .createUserWithEmailAndPassword(email, password);
+  }
+  login(email: string, password: string) {
+    return this.firebaseAuth.auth
+      .signInWithEmailAndPassword(email, password);
+  }
+  logout(){
+    return this.firebaseAuth.auth
+      .signOut();
+  }
 }
