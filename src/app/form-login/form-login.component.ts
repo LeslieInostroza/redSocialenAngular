bd77e4283e79828fbc53d775dd4b392515bb8af3
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-09-12 10:34:00 -0300

    registro de ususario

--- src/app/form-login/form-login.component.ts
+++ src/app/form-login/form-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-form-login',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent implements OnInit {
+  formLogin:FormGroup;
 
-  constructor() { }
-
+  constructor(private formBuilder:FormBuilder, private authService:AuthService, public snackBar: MatSnackBar) { this.createFormLogin();
+  }
+  
   ngOnInit() {
   }
+  createFormLogin(){
+    this.formLogin = this.formBuilder.group({
+      email:['', Validators.compose([Validators.required, Validators.email])],
+      password:['', Validators.compose([Validators.required, Validators.minLength(4)])]
+    });
+}
+onRegister(){
+  this.authService.signup(this.formLogin.value.email, this.formLogin.value.password)
+  .then(()=>{
+    //Registro exitoso, celebremos esto!
+  })
+  .catch(()=>{
+    //Algo salió mal, avisemos mejor para que reintente
+    this.snackBar.open('Error de registro, trata otra vez'
+                        ,null/*No necesitamos botón en el aviso*/
+                        ,{
+                          duration : 3000
+                        });
+  });
+}
 
+onLogin(){
+  this.authService.login(this.formLogin.value.email, this.formLogin.value.password)
+  .then(()=>{
+    //Login exitoso, así que celebramos con el usuario (?)
+  })
+  .catch(()=>{
+    //Algo salió mal, avisemos mejor para que reintente
+    this.snackBar.open('Error al tratar de iniciar sesión, trata otra vez'
+                        ,null/*No necesitamos botón en el aviso*/
+                        ,{
+                          duration : 3000
+                        });
+  });
+}
 }
