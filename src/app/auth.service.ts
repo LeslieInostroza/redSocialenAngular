8895c36af5bd7dfe74ded633c7c46cb8137a0ced
LeslieInostroza <leslie.ino@gmail.com>
2018-09-12 17:12:22 -0300

    se empieza login con google

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,18 @@ export class AuthService {
   logout(){
     return this.firebaseAuth.auth
       .signOut();
+  }
+  loginGoogle(){
+    return  new Promise<any>((resolve, reject)=>{
+      let provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('profile');
+      provider.addScope('email');
+      this.firebaseAuth.auth
+      .signInWithPopup(provider)
+      .then(response => {
+        resolve (response)
+      })
+    })
+    
   }
 }
--- src/app/google-login/google-login.component.ts
+++ src/app/google-login/google-login.component.ts
@@ -11,5 +11,5 @@ export class GoogleLoginComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  
 }
