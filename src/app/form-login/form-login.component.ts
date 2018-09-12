819acba33670b4d93cfa6a17d041f69b2505ed02
LeslieInostroza <leslie.ino@gmail.com>
2018-09-12 13:32:47 -0300

    se agrega router en promesas de inicio sesion

--- src/app/form-login/form-login.component.ts
+++ src/app/form-login/form-login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-login',
@@ -11,7 +12,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class FormLoginComponent implements OnInit {
   formLogin:FormGroup;
 
-  constructor(private formBuilder:FormBuilder, private authService:AuthService, public snackBar: MatSnackBar) { this.createFormLogin();
+  constructor(private formBuilder:FormBuilder, private authService:AuthService, public snackBar: MatSnackBar, private router: Router) { this.createFormLogin();
   }
   
   ngOnInit() {
@@ -21,35 +22,36 @@ export class FormLoginComponent implements OnInit {
       email:['', Validators.compose([Validators.required, Validators.email])],
       password:['', Validators.compose([Validators.required, Validators.minLength(4)])]
     });
-}
-onRegister(){
-  this.authService.signup(this.formLogin.value.email, this.formLogin.value.password)
-  .then(()=>{
+  }
+  onRegister(){
+    this.authService.signup(this.formLogin.value.email, this.formLogin.value.password)
+    .then(()=>{
     //Registro exitoso, celebremos esto!
-  })
-  .catch(()=>{
+    })
+    .catch(()=>{
     //Algo salió mal, avisemos mejor para que reintente
     this.snackBar.open('Error de registro, trata otra vez'
                         ,null/*No necesitamos botón en el aviso*/
                         ,{
                           duration : 3000
                         });
-  });
-}
+    });
+  }
 
-onLogin(){
-  this.authService.login(this.formLogin.value.email, this.formLogin.value.password)
-  .then(()=>{
+  onLogin(){
+    this.authService.login(this.formLogin.value.email, this.formLogin.value.password)
+    .then(()=>{
     //Login exitoso, así que celebramos con el usuario (?)
-  })
-  .catch(()=>{
+      this.router.navigate(['/muro']);
+    })
+    .catch(()=>{
     //Algo salió mal, avisemos mejor para que reintente
     this.snackBar.open('Error al tratar de iniciar sesión, trata otra vez'
                         ,null/*No necesitamos botón en el aviso*/
                         ,{
                           duration : 3000
                         });
-  });
-}
+    });
+  }
 
 }
